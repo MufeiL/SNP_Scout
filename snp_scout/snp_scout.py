f3f3e25f826de280bd2285a767f8ee511fda2449
Sally Ha <sallyyha@gmail.com>
2024-05-15 12:44:17 -0700

    Update snp_scout.py

--- snp_scout/snp_scout.py
+++ snp_scout/snp_scout.py
@@ -1,8 +1,26 @@
 import argparse
 
+def reference_matching(matches): #check if matches are the same as reference base
+  for char in matches:
+    if char not in (',', '.'):
+      return False
+  return True
+
 #function to perform variant calling
 def snp_scout(m_file):
-  pass
+  with open(m_file, "r") as f:
+    for line in f:
+      columns = line.strip().split('\t') #split line into columns
+      chromosome = columns[0]
+      position = columns[1]
+      reference_base = columns[2]
+      #coverage = columns[3]
+      matches = columns[4]
+      #states = columns[5]
+      if matches == True:
+        continue
+      else:
+        pass
 
 #function to parse user-given arguments
 def main()
