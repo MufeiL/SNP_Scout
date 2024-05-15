f1ce900b3aa408e1ee2fcf86b9d99d8c786cf455
Sally Ha <sallyyha@gmail.com>
2024-05-15 12:25:38 -0700

    Update snp_scout.py
    added main() function

--- snp_scout/snp_scout.py
+++ snp_scout/snp_scout.py
@@ -1,1 +1,15 @@
+import argparse
 
+#function to perform variant calling
+def snp_scout(m_file):
+  pass
+
+#function to parse user-given arguments
+def main()
+parser = argparse.ArgumentParser(prog = "SNP Scout", description = "Command-line script to perform variant calling")
+
+#Add required inputs
+parser.add_argument("mpileup", help = "SAMtools mpileup file", type = str)
+
+args = parser.parse_args()
+pileup_file = args.mpileup #obtain name of pileup file
