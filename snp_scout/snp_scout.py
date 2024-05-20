f7f6000cbfdc5c7c3577c44c93b7e13e2f2bfad4
Sally Ha <sallyyha@gmail.com>
2024-05-20 13:42:29 -0700

    Update snp_scout.py
    added new options for file

--- snp_scout/snp_scout.py
+++ snp_scout/snp_scout.py
@@ -1,7 +1,7 @@
 import argparse
 
 #function to perform variant calling
-def snp_scout(m_file, var_freq, min_cov, out_file = None):
+def snp_scout(m_file, var_freq, min_cov, min_reads2, min_homo, out_file = None):
     output_handle = None
     if out_file:
         output_handle = open(out_file, "w")
@@ -26,15 +26,15 @@ def snp_scout(m_file, var_freq, min_cov, out_file = None):
                 # Calculate variant frequency
                 tot_var = sum(1 for base in reads if base not in (',', '.', '!', '$', '^')) #total variants per line
                 freq = tot_var / max(1, coverage)
-                if freq >= var_freq and coverage >= min_cov: #to consider for variant calling
-                    alt_allele_str = ','.join(alt_alleles) if alt_alleles else "N/A" #get alt allele
+                if freq >= var_freq and coverage >= min_cov and tot_var >= min_reads2: #to consider for variant calling
+                    alt_allele_str = ','.join(alt_alleles) if alt_alleles else "N/A" #get alt allele(s)
                     
                     if alt_allele_str == reference_base: #get genotype (assuming we only have 1 alt base)
-                        gt = "0/0"
-                    elif alt_allele_str != reference_base and (',' in reads or '.' in reads):
-                        gt = "0/1"
+                        gt = "0/0" #homozygous
+                    elif alt_allele_str != reference_base and (',' in reads or '.' in reads) and freq < min_homo:
+                        gt = "0/1" #heterozygous
                     else:
-                        gt = "1/1"
+                        gt = "1/1" #homozygous for alt
 
                     if output_handle:
                         output_handle.write(f"{chromosome}\t{position}\t{reference_base}\t{alt_allele_str}\tGT\t{gt}\n")
@@ -51,14 +51,18 @@ def main():
     parser.add_argument("mpileup", help = "SAMtools mpileup file", type = str)
     parser.add_argument("-m", "--min_var_freq", help = "Minimum variant frequency", type = float, default = 0.01)
     parser.add_argument("-c", "--min_coverage", help = "Minimum read depth", type = float, default = 8)
+    parser.add_argument("-r", "--min_reads2", help = "Minimum supporting reads for variants", type = int, default = 2)
+    parser.add_argument("-f", "--min_freq_for_homo", help = "Minimum frequency for homozygotes", type = float, default = 0.75)
     parser.add_argument("-o", "--out", help = "Write output to file", type = str)
     
     args = parser.parse_args()
     pileup_file = args.mpileup #name of pileup file
     var_freq = args.min_var_freq
     min_cov = args.min_coverage
+    min_reads2 = args.min_reads2
+    min_homo = args.min_freq_for_homo
     out_file = args.out #name of output file (if included)
-    snp_scout(pileup_file, var_freq, min_cov, out_file)
+    snp_scout(pileup_file, var_freq, min_cov, min_reads2, min_homo, out_file)
 
 if __name__ == "__main__":
     main()
