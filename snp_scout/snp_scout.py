3b59b51880e2f69fe1fad7f03cbba53289a4711a
Sally Ha <sallyyha@gmail.com>
2024-05-24 16:00:06 -0700

    Made vcf output more descriptive

--- snp_scout/snp_scout.py
+++ snp_scout/snp_scout.py
@@ -5,12 +5,12 @@ def snp_scout(m_file, var_freq, min_cov, min_reads2, min_homo, out_file = None):
     output_handle = None
     if out_file:
         output_handle = open(out_file, "w")
-        header = "\t".join(["#CHROM", "POS", "REF", "ALT", "FORMAT", "SAMPLE1"]) + "\n"
+        header = "\t".join(["#CHROM", "POS", "ID", "REF", "ALT", "FILTER", "INFO", "FORMAT", "SAMPLE1"]) + "\n"
         output_handle.write(header)
     else:
-        print("\t".join(["#CHROM", "POS", "REF", "ALT", "FORMAT", "SAMPLE1"]))
+        print("\t".join(["#CHROM", "POS", "ID", "REF", "ALT", "FILTER", "INFO", "FORMAT", "SAMPLE1"]))
     
-    nucs = ['A', 'T', 'C', 'G']
+    nucs = ['A', 'T', 'C', 'G'] #nucleotide bases
     
     with open(m_file, "r") as f:
         for line in f:
@@ -24,24 +24,33 @@ def snp_scout(m_file, var_freq, min_cov, min_reads2, min_homo, out_file = None):
 
                 # Get alternate alleles
                 alt_alleles = set(base.upper() for base in reads if base.upper() != reference_base.upper() and base.upper() in nucs)
-
+                het = 0 #labels for what type of sample/gt we have
+                homo = 0
+                wt = 0
+                filt = "fail" #filter
                 # Calculate variant frequency
                 tot_var = sum(1 for base in reads if base.upper() != reference_base.upper() and base.upper() in nucs) #total variants per line
                 freq = tot_var / max(1, coverage) #variant frequency
                 if freq >= var_freq and coverage >= min_cov and tot_var >= min_reads2: #to consider for variant calling
+                    filt = "PASS"
                     alt_allele_str = ','.join(alt_alleles) if alt_alleles else "N/A" #get alt allele(s)
                     
                     if alt_allele_str == reference_base: #get genotype
                         gt = "0/0" #homozygous
+                        wt +=1
                     elif alt_allele_str != reference_base and (',' in reads or '.' in reads) and freq < min_homo:
                         gt = "0/1" #heterozygous
+                        het +=1
                     else:
                         gt = "1/1" #homozygous for alt
-
+                        homo +=1
+                    
+                    info_str = f"WT={wt};HET={het};HOM={homo}" #string for info header
+                    #WT=0;HET=0;HOM=3
                     if output_handle:
-                        output_handle.write(f"{chromosome}\t{position}\t{reference_base}\t{alt_allele_str}\tGT:SDP:FREQ\t{gt}:{coverage}:{freq*100}%\n")
+                        output_handle.write(f"{chromosome}\t{position}\t.\t{reference_base}\t{alt_allele_str}\t{filt}\t{info_str}\tGT:SDP:FREQ\t{gt}:{coverage}:{freq*100}%\n")
                     else:
-                        print(f"{chromosome}\t{position}\t{reference_base}\t{alt_allele_str}\tGT:SDP:FREQ\t{gt}:{coverage}:{freq*100}%")
+                        print(f"{chromosome}\t{position}\t.\t{reference_base}\t{alt_allele_str}\t{filt}\t{info_str}\tGT:SDP:FREQ\t{gt}:{coverage}:{freq*100}%")
     if output_handle:
         output_handle.close()
 
